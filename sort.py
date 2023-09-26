1f9eec42306911c4979b6d846cab01a893a89f92
loha3000 <amp.lesha@gmail.com>
2023-09-26 14:26:25 +0300

    sort module

---
+++ sort.py
@@ -0,0 +1,43 @@
+"""
+модуль сортировки
+"""
+
+def sort(sorting_sequence, place_number=0):
+    """
+    метод RADIX LSD
+    или
+    поразрядная по наименее значимой цифре
+    """
+    def get_digit_at_place_number(gived_element):
+        if len(gived_element) < place_number + 1:
+            return ''  # минимальное значение при отсутствии разряда
+        return gived_element[-(place_number + 1)]
+
+    sort_buffer = [[sorting_sequence[0]]]  # заполнение начальным значением
+    for element in sorting_sequence[1:]:
+        for index, sorted_subsequence in enumerate(sort_buffer):  # перебор сохранённых значений
+            if (get_digit_at_place_number(element) <
+                    get_digit_at_place_number(sorted_subsequence[0])):
+                # меньше текущего
+                sort_buffer.insert(index, [element])
+                break
+            elif (get_digit_at_place_number(element) ==
+                    get_digit_at_place_number(sorted_subsequence[0])):
+                # равно текущему
+                sorted_subsequence.append(element)
+                break
+        if (get_digit_at_place_number(element) >
+                get_digit_at_place_number(sorted_subsequence[0])):
+            # больше последнего
+            sort_buffer.append([element])
+    # объединение строк
+    sorted_sequence = [element for subsequence in sort_buffer for element in subsequence]
+    if place_number < max(map(len, sorting_sequence)):  # проверка наличия большего разряда
+        sorted_sequence = sort(sorted_sequence, place_number + 1)
+    return sorted_sequence
+
+
+
+if __name__ == '__main__':
+    from random import randint
+    print(sort(list(map(str, tuple([randint(0, 10**randint(0, 4)) for i in range(20)])))))
